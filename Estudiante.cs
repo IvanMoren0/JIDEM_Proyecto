873d4e8c22d5819f87a5a996607c4f5f24be253d
unknown <Sala300@TUNDFPCGE10F362.FormacionSena.RED>
2019-08-06 18:02:39 -0500

    Initial commit

---
+++ Estudiante.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JIDEM_Proyecto
+{
+    class Estudiante
+    {
+
+        #region Atributos 
+        #endregion
+        #region Propiedades
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NDocumento { get; set; }
+        public int Curso { get; set; }
+        #endregion
+
+        #region Metodos 
+        public void IngresarDatosEstudiante()
+        {
+
+            Console.WriteLine("Ingrese Nombre");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese Apellido");
+            Apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese numero documento");
+            NDocumento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese Curso");
+            Curso  = int.Parse(Console.ReadLine());
+        }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("INSCRITO");
+            Console.WriteLine();
+
+            Console.WriteLine($"Nombre:{Nombre}");
+            Console.WriteLine($"Apellido:{Apellido}");
+            Console.WriteLine($"NDocumento:{NDocumento}");
+            Console.WriteLine($"Curso:{Curso}");
+
+
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JIDEM_Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante e = new Estudiante();
+            Console.WriteLine(----------"Ingrese Datos"------------);
+            e.IngresarDatosEstudiante();
+            e.ImprimirDatos();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
